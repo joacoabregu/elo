146fcdd451ad454394a439eaf439ff0872462807
Joaquin Abregu <abregujoaquin@gmail.com>
2024-12-24 16:32:28 -0300

    Feat: format files

--- app/sports/page.tsx
+++ app/sports/page.tsx
@@ -1,14 +1,16 @@
-import { SPORTS } from '@/common/constants/DB';
-import { getDBClient } from '@/utils/supabase/server';
+import { SPORTS } from "@/common/constants/DB";
+import { getDBClient } from "@/utils/supabase/server";
 
 export default async function Countries() {
-const {data} = await (await getDBClient()).from(SPORTS).select();
+	const { data } = await (await getDBClient()).from(SPORTS).select();
 
-  return <div>
-    <ul>
-        {data?.map((sport) => (
-            <li key={sport.id}>{sport.name}</li>
-        ))}
-    </ul>
-  </div>
+	return (
+		<div>
+			<ul>
+				{data?.map((sport) => (
+					<li key={sport.id}>{sport.name}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,73 +1,69 @@
 import { createServerClient } from "@supabase/ssr";
-import { SupabaseClient } from '@supabase/supabase-js';
+import { SupabaseClient } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
-import {Database} from '@/index';
-
+import { Database } from "@/index";
 
 export const createClient = async () => {
-  const cookieStore = await cookies();
+	const cookieStore = await cookies();
 
-  return createServerClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) => {
-              cookieStore.set(name, value, options);
-            });
-          } catch (error) {
-            // The `set` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
-      },
-    },
-  );
+	return createServerClient<Database>(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		{
+			cookies: {
+				getAll() {
+					return cookieStore.getAll();
+				},
+				setAll(cookiesToSet) {
+					try {
+						cookiesToSet.forEach(({ name, value, options }) => {
+							cookieStore.set(name, value, options);
+						});
+					} catch (error) {
+						// The `set` method was called from a Server Component.
+						// This can be ignored if you have middleware refreshing
+						// user sessions.
+					}
+				},
+			},
+		},
+	);
 };
 
-export type ServerClient = SupabaseClient<Database, "public", Database["public"]>;
+export type ServerClient = SupabaseClient<
+	Database,
+	"public",
+	Database["public"]
+>;
 
 let supabaseClient: ServerClient | null = null;
 
-export const getDBClient = async () =>
-{
-    if ( !supabaseClient )
-    {
-        const cookieStore = await cookies();
-        supabaseClient = createServerClient(
-            process.env.NEXT_PUBLIC_SUPABASE_URL!,
-            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-            {
-                cookies: {
-                    getAll ()
-                    {
-                        return cookieStore.getAll();
-                    },
-                    setAll ( cookiesToSet )
-                    {
-                        try
-                        {
-                            cookiesToSet.forEach( ( { name, value, options } ) =>
-                                cookieStore.set( name, value, options )
-                            );
-                        } catch
-                        {
-                            // The `setAll` method was called from a Server Component.
-                            // This can be ignored if you have middleware refreshing
-                            // user sessions.
-                        }
-                    },
-                },
-            }
-        );
-    }
+export const getDBClient = async () => {
+	if (!supabaseClient) {
+		const cookieStore = await cookies();
+		supabaseClient = createServerClient(
+			process.env.NEXT_PUBLIC_SUPABASE_URL!,
+			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+			{
+				cookies: {
+					getAll() {
+						return cookieStore.getAll();
+					},
+					setAll(cookiesToSet) {
+						try {
+							cookiesToSet.forEach(({ name, value, options }) =>
+								cookieStore.set(name, value, options),
+							);
+						} catch {
+							// The `setAll` method was called from a Server Component.
+							// This can be ignored if you have middleware refreshing
+							// user sessions.
+						}
+					},
+				},
+			},
+		);
+	}
 
-    return supabaseClient;
+	return supabaseClient;
 };
-
