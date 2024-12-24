daf864d8dbee06d5e1fe82fd291497daf7aac5ad
Joaquin Abregu <abregujoaquin@gmail.com>
2024-12-24 16:00:47 -0300

    Feat: Add server client singletone

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,10 +1,10 @@
 import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
-import { createClient } from "@/utils/supabase/server";
+import { getDBClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 
 export default async function ProtectedPage() {
-  const supabase = await createClient();
+  const supabase = await getDBClient();
 
   const {
     data: { user },
--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -3,10 +3,10 @@ import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import Link from "next/link";
 import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
-import { createClient } from "@/utils/supabase/server";
+import { getDBClient } from "@/utils/supabase/server";
 
 export default async function AuthButton() {
-  const supabase = await createClient();
+  const supabase = await getDBClient();
 
   const {
     data: { user },
