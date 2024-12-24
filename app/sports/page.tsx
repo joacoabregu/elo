a877f137e1c75efb3eed858032568d2ec3c48ba6
Joaquin Abregu <abregujoaquin@gmail.com>
2024-12-24 16:13:15 -0300

    Feat: Add DB constants file

--- app/sports/page.tsx
+++ app/sports/page.tsx
@@ -1,7 +1,8 @@
+import { SPORTS } from '@/common/constants/DB';
 import { getDBClient } from '@/utils/supabase/server';
 
 export default async function Countries() {
-const {data} = await (await getDBClient()).from("Sports").select();
+const {data} = await (await getDBClient()).from(SPORTS).select();
 
   return <div>
     <ul>
