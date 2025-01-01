5c94136cc7401297e7c0a820e800620a3ae126e5
Joaquin Abregu <abregujoaquin@gmail.com>
2025-01-01 20:03:26 -0300

    feat: add style for sports page

--- app/sports/page.tsx
+++ app/sports/page.tsx
@@ -1,16 +1,24 @@
 import { SPORTS } from "@/common/constants/DB";
 import { getDBClient } from "@/utils/supabase/server";
+import Link from "next/link";
 
 export default async function Countries() {
 	const { data } = await (await getDBClient()).from(SPORTS).select();
 
 	return (
-		<div>
-			<ul>
+		<div className="container mx-auto p-4">
+			<h1 className="text-4xl font-bold mb-6 text-center">Sports</h1>
+			<div className="grid grid-cols-5">
 				{data?.map((sport) => (
-					<li key={sport.id}>{sport.name}</li>
+					<Link
+						key={sport.id}
+						href={`/sports/${sport.name}`}
+						className="block p-2 text-blue-600 capitalize  text-center"
+					>
+						{sport.name}
+					</Link>
 				))}
-			</ul>
+			</div>
 		</div>
 	);
 }
